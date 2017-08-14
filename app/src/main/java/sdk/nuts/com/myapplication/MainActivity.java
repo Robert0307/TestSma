01aecdb6f5852f7234538e51520b0c23efa91ebe
张盼盼 <jackzppzpp@gmail.com>
2017-08-14 16:10:07 +0800

    修改了代码

--- app/src/main/java/sdk/nuts/com/myapplication/MainActivity.java
+++ app/src/main/java/sdk/nuts/com/myapplication/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Hello.helloWord();
         Hello.helloNuts();
+        //测试修改
     }
 
     @Override
