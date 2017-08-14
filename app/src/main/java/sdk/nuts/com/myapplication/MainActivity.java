9c95a0445b113d47292e4eabcbc75df7802670aa
张盼盼 <jackzppzpp@gmail.com>
2017-08-14 15:34:53 +0800

    测试

--- app/src/main/java/sdk/nuts/com/myapplication/MainActivity.java
+++ app/src/main/java/sdk/nuts/com/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package sdk.nuts.com.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+    }
+
 }
