8086f9404c6ba8a261c0e4a1d28daeb809e98757
张盼盼 <jackzppzpp@gmail.com>
2017-08-14 15:53:41 +0800

    测试

---
+++ app/src/main/java/sdk/nuts/com/myapplication/Hello.java
@@ -0,0 +1,18 @@
+package sdk.nuts.com.myapplication;
+
+import android.util.Log;
+
+/**
+ * 功能:
+ * Created by zhangpanpan on 2017/8/14
+ * 努力做得更好!
+ */
+
+public class Hello {
+
+    private static final String TAG = "Hello";
+
+    public static void helloWord(){
+        Log.d(TAG, "helloWord");
+    }
+}
